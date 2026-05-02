c4a5236e2d7c0e0ca439faa6a68ddbe60bae8f32
Iyundhu Kennedy <kennedyiyundhu@gmail.com>
2026-05-02 10:11:53 +0300

    Add StatisticsScreen class to statistics_screen.dart

--- Flutter app/statistics_screen.dart
+++ Flutter app/statistics_screen.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'api_service.dart';
+
+class StatisticsScreen extends StatefulWidget {
+  const StatisticsScreen({super.key});
