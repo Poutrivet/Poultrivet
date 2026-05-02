bf84cc237dfd90db04076fa1a24c9e847f1515ee
Iyundhu Kennedy <kennedyiyundhu@gmail.com>
2026-05-02 10:18:21 +0300

    Add background and card colors to statistics screen

--- Flutter app/statistics_screen.dart
+++ Flutter app/statistics_screen.dart
@@ -11,3 +11,5 @@ class StatisticsScreen extends StatefulWidget {
 class _StatisticsScreenState extends State<StatisticsScreen> {
  static const Color primaryGreen = Color(0xFF2ECC71);
   static const Color lightGreen = Color(0xFFE8F8F0);
+   static const Color bgColor = Color(0xFFF0F2F0);
+  static const Color cardColor = Colors.white;
