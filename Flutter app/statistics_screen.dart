41e3bcd9cb733cbc3026718a942d79de35c7fe73
Iyundhu Kennedy <kennedyiyundhu@gmail.com>
2026-05-11 23:59:21 +0300

    Update statistics_screen.dart

--- Flutter app/statistics_screen.dart
+++ Flutter app/statistics_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'api_service.dart';
 
-class StatisticsScreen extends StatefulWidget {
+class StatisticsScreen extends StatefulWidget{
 const StatisticsScreen({super.key});
 
 @override
