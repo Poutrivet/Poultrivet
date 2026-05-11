b7fd42a736dc4f1c8cdca42cfe903b0c11e8cc3d
Iyundhu Kennedy <kennedyiyundhu@gmail.com>
2026-05-12 00:00:16 +0300

    Update statistics_screen.dart

--- Flutter app/statistics_screen.dart
+++ Flutter app/statistics_screen.dart
@@ -7,7 +7,7 @@ const StatisticsScreen({super.key});
 
 @override
 State<StatisticsScreen> createState() => _StatisticsScreenState();
-}
+ }
 class _StatisticsScreenState extends State<StatisticsScreen> {
 static const Color primaryGreen = Color(0xFF2ECC71);
 static const Color lightGreen = Color(0xFFE8F8F0);
