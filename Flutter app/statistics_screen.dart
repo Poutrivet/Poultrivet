eb784060ca066b67a0c32a5394d9ffca89875a91
Iyundhu Kennedy <kennedyiyundhu@gmail.com>
2026-05-29 10:44:20 +0300

    Add empty line for code formatting in statistics_screen.dart

--- Flutter app/statistics_screen.dart
+++ Flutter app/statistics_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart ';
 import 'api_service.dart';
 
+
 class StatisticsScreen extends StatefulWidget{
 const StatisticsScreen({super.key});
 @override
